01cbbd59588dc20be316628e9ba80310cc66d585
Aaron Mwarabu <mwarabuaaron01@gmail.com>
2022-01-03 18:18:23 +0300

    added the toggle effects on icons

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    // Toggle effects on the icons
+    $(".clickable").click(function () {
+        $(".design-hidden").toggle();
+        $(".design-visible").toggle();
+    });
+    $(".dev-clickable").click(function () {
+        $(".development-hidden").toggle();
+        $(".development-visible").toggle();
+    });
+    $(".management-clickable").click(function () {
+        $(".management-hidden").toggle();
+        $(".management-visible").toggle();
+    });
+})
